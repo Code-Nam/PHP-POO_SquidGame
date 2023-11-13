1c0205130abeb258a7ae49b6e56e3460f230c72e
AnnamTRAN <114738120+AnnamTRAN@users.noreply.github.com>
2023-11-13 14:34:05 +0100

    🎉 - Initial commit

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php
+
+$Personnages = array(
+    [
+			'nom' => "Seong Gi-hun",
+			'marbles' => 15,
+			'loss' => 2,
+			'gain' => 1,
+			'battle cry' => "Can't you hear me? This isn't right! We shouldn't be killing each other like this!"
+    ],
+    [
+			'nom'	=> 'Kang Sae-byeok',
+    ]
+);
+
+?>
